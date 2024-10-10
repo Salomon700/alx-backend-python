af31f38343bc37bcb6c41b402c190b22f4573c23
Salomon700 <ntwalisolomon9@gmail.com>
2024-10-10 23:15:07 +0200

    task completed

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""
+Basic annotations for variables
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    return sum of floats
+    """
+    return a + b
---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""
+Basic annotation concat 
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    return sum of two strings
+    """
+    return str1 + str2
